b0f5e6dd9f4357c6f54f3b50beb49e50e77d2e3a
Andrew Desmarais <andydes@amazon.com>
2023-04-07 16:21:47 -0400

    Update index.tsx 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '} - hooray 5
+              By{' '} - hooray 6
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
